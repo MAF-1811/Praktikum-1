008092e51575d43936c120217f2b68bb8040238a
M.azzam fikri <100785864+MAF-1811@users.noreply.github.com>
2022-03-03 12:06:36 +0700

    Add files via upload

---
+++ user.php
@@ -0,0 +1,15 @@
+<?php
+    //Data User
+    $nama = 'Muhammad Azzam Fikri';
+    $umur = 21;
+    $tb = 170;
+    $bb = 51;
+
+    echo 'Nama : '.$nama;
+    echo '<br/> umur : '.$umur.' tahun';
+    echo '<br/> tinggi badan : '.$tb.' cm';
+    echo '<br/> berat badan : '.$bb.' kg';
+
+    echo "<br/> Hai $nama ApaKabar?";
+
+?>
